775bf6d61070a58c98ca8c86bbdcc0aa6d23ad74
Alex Seiger <awseiger@gmail.com>
2021-06-17 00:03:40 -0400

    WPM Progressbar OLED display

--- keyboards/gingham/gingham.c
+++ keyboards/gingham/gingham.c
@@ -19,20 +19,22 @@
 uint8_t send_data;
 
 void matrix_init_kb(void) {
-    // Due to the way the port expander is setup both LEDs are already outputs. This is set n matrix.copy
-    //Turn the red LED on as power indicator.
-    send_data = 0x10;
-    i2c_writeReg((PORT_EXPANDER_ADDRESS << 1), 0x09, &send_data, 1, 20);
+    // // Due to the way the port expander is setup both LEDs are already outputs. This is set n matrix.copy
+    // //Turn the green LED on as power indicator.
+    // send_data = 0x08;
+    // i2c_writeReg((PORT_EXPANDER_ADDRESS << 1), 0x09, &send_data, 1, 20);
 
     matrix_init_user();
 }
 
 void led_set_kb(uint8_t usb_led) {
     // Bit 3 is Green LED, bit 4 is Red LED.
+    send_data = 0x00;
     if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        send_data = 0x18;
-    } else {
-        send_data = 0x10;
+        send_data |= 0x10;
+    }
+    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+        send_data |= 0x08;
     }
     i2c_writeReg((PORT_EXPANDER_ADDRESS << 1), 0x09, &send_data, 1, 20);
 
--- keyboards/gingham/rules.mk
+++ keyboards/gingham/rules.mk
@@ -34,3 +34,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 
 CUSTOM_MATRIX = lite
+OLED_DRIVER_ENABLE = yes
+WPM_ENABLE = yes
