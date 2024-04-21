d18110747eae91f9714cdd424b690f8bf66e5e3c
Alex Seiger <awseiger@gmail.com>
2024-04-21 01:11:48 -0400

    Adding cyberdeck_keyboard_v2

---
+++ keyboards/cyberdeck_keyboard_v2/config.h
@@ -0,0 +1,27 @@
+// Copyright 2024 Alex Seiger (@Alex Seiger)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+
+#define I2C_DRIVER I2CD0
+#define I2C1_SDA_PIN GP20
+#define I2C1_SCL_PIN GP21
+
+#define LED_CAPS_LOCK_PIN GP25
